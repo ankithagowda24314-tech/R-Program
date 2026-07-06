a5fc5acfd00d78cef5a22bce3078790cb64cb4f5
Your Name <ankithagowda24314@gmail.com>
2026-07-06 10:58:20 +0530

    Initial commit 6-7-26

---
+++ session.R
@@ -0,0 +1,39 @@
+
+
+# Question 2
+
+x <- 15
+y <- 7
+z <- 3
+
+result <- (x+y)*z-(x/y)
+
+print(result)
+
+
+# Question 3
+
+a <- 10
+b <- a
+a <- 20
+
+print(b)
+
+# Question 4
+
+a <- 10
+b = 20
+30 ->  c
+
+rm(b)
+
+print(a)
+print(c)
+
+
+var1 <- "HELLO WORLD"
+var2 <- "3.14159"
+var3 <- "42L"
+var4 <- "FALSE"
+var5 <- ""
+
