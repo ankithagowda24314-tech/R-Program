af74f2b8da0144edf05eb7de853513f8c5d3d9f1
Your Name <ankithagowda24314@gmail.com>
2026-07-13 10:54:13 +0530

    question 14

--- question 14.R
+++ question 14.R
@@ -4,3 +4,13 @@ round(3.14159,2)
 ceiling(5.2)
 floor(7.9)
 17 %% 5
+
+# Question 15
+seq <- seq(1, 20, by = 2)
+# sum of all nos in sequence
+sum(seq)
+# product of first 5 nos in sequence
+prod(seq[1:5])
+
+
+
